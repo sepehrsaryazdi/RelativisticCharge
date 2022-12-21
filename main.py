c2857dc0e708376c4760d05cb957b10a93e084ba
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2022-12-21 21:49:10 +1100

    Added mouse dragging for charge

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from collections import deque
 
+
+class Charge:
+    def __init__(self, x0):
+        assert len(x0) == 2
+        self.max_num_history = 100
+        self.previous_positions = deque([x0]*self.max_num_history)
+        self.updating_pos = False
+    
+    def update_position(self, x):
+        self.previous_positions.popleft()
+        self.previous_positions.append(x)
 
 
 
@@ -12,7 +24,9 @@ class Visualise(tk.Frame):
         self.root.title('Relativistic Charge Visualisation')
         self.root.geometry("1280x520")
         super().__init__(self.root)
-        #self.win = tk.Toplevel()
+        self.num_charges = 1
+        self.initial_points = [np.array([np.random.uniform(-1,1),0]) for i in range(self.num_charges)]
+        self.charges = [Charge(self.initial_points[i]) for i in range(self.num_charges)]
         self.updating_pos = False
         self.figure = plt.Figure(figsize=(7, 5), dpi=100)
         self.figure.canvas.mpl_connect('motion_notify_event', lambda e: self.update_charge_pos(e))
@@ -21,36 +35,49 @@ class Visualise(tk.Frame):
         self.ax = self.figure.add_subplot(111)
         self.chart_type = FigureCanvasTkAgg(self.figure, self.root)
         self.chart_type.get_tk_widget().pack()
-        self.ax.plot(np.linspace(0,1,5), np.linspace(0,1,5))
+        self.draw_charge_positions()
+        # self.ax.plot(np.linspace(0,1,5), np.linspace(0,1,5))
+    
+    def draw_charge_positions(self):
+        self.ax.clear()
+        self.ax.set_xlim(-1,1)
+        self.ax.set_ylim(-1,1)
+        self.ax.set_axis_off()
+        
+        for q in self.charges:
+            #self.ax.set_title(f"{q.previous_positions[-1]}")
+            self.ax.scatter(*q.previous_positions[-1])
+        
         self.chart_type.draw()
 
-    
-    # def matplotlib_to_tkinter(self, x):
-    #     x = np.array([[x[0]],[x[1]]])
-    #     mat1 = [-3.031443579456372, -4.828833563156737]
-    #     tk1 = [-66., 87.]
-    #     mat2 = [10.675953475476799, -0.25732417952460196]
-    #     tk2 = [272., 7.]
-    #     M = [[mat1[0], mat1[1], 0, 0], [0, 0, mat1[0], mat1[1]], [mat2[0], mat2[1], 0, 0], [0, 0, mat2[0], mat2[1]]]
-    #     T = [[tk1[0]], [tk1[1]], [tk2[0]], [tk2[1]]]
-    #     M = np.array(M)
-    #     T = np.array(T)
-    #     solution = np.matmul(np.linalg.inv(M), T).T.flatten()
-    #     A = np.array([[solution[0], solution[1]], [solution[2], solution[3]]])
-    #     kt_coords = np.matmul(A, x)
-    #     kt_coords = kt_coords.T.flatten()
-    #     abs_coord_x = self.chart_type.get_tk_widget().winfo_x() + kt_coords[
-    #         0] + 0.5 * self.chart_type.get_tk_widget().winfo_width()
-    #     abs_coord_y = self.chart_type.get_tk_widget().winfo_y() + kt_coords[
-    #         1] + 0.5 * self.chart_type.get_tk_widget().winfo_height()
-    #     return [abs_coord_x, abs_coord_y]
+
 
     def toggle_updating(self, event):
         self.updating_pos = not self.updating_pos
+        if not self.updating_pos:
+            for q in self.charges:
+                q.updating_pos = False
 
     def update_charge_pos(self, event):
-        if self.updating_pos:
-            print(event)
+        x_matplot = event.xdata
+        y_matplot = event.ydata
+        
+        x = np.array([x_matplot,y_matplot])
+        for q in self.charges:
+            if self.updating_pos or q.updating_pos:
+                #print(np.linalg.norm(np.array([x_matplot,y_matplot])-q.previous_positions[-1]))
+                if np.linalg.norm(x-q.previous_positions[-1]) < 0.5:
+                    q.updating_pos = True
+                q.update_position(x)
+                self.draw_charge_positions()
+
+
+                    
+                    #print(x_matplot,y_matplot)
+                    #print(np.linalg.norm(np.array([x_matplot,y_matplot])-q.previous_positions[-1]))
+
+        
+            
         #x = self.root.winfo_pointerx()
         #y = self.root.winfo_pointery()
         #print(x,y)
